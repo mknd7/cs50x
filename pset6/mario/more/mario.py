d64ab9ff63235f5501d5aa7939cab9abb22fae44
Mukund Vijayaraghavan <mkndviji@gmail.com>
2020-05-03 08:19:38 +0530

    Start pset6

---
+++ pset6/mario/more/mario.py
@@ -0,0 +1,12 @@
+from cs50 import get_int
+
+while True:
+    height = get_int("Height: ")
+    if height >= 1 and height <= 8:
+        break
+
+for i in range(height):
+    print(" " * (height - 1 - i), end='')
+    print("#" * (i + 1), end='')
+    print(" " * 2, end='')
+    print("#" * (i + 1))
